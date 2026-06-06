48478c3166b2a742cbb62f60e23cb3d2227e5a4d
Rocoi <roordguez@gmail.com>
2026-06-06 10:39:25 +0200

    ultimos cambios con validator y CRUD

--- src/models/Admin.js
+++ src/models/Admin.js
@@ -1,18 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const AdminSchema = new mongoose.Schema({
-<<<<<<< HEAD
-  nombre:    { type: String, required: true },
-  email:     { type: String, required: true, unique: true },
-  password:  { type: String, required: true },
-  rol:       { type: String, default: 'admin' }
-=======
   nombre: { type: String, required: true, trim: true },
   apellidos: { type: String, required: true, trim: true },
-  email: { type: String, required: true, unique: true, trim: true},
+  email: { type: String, required: true, unique: true, trim: true },
   password: { type: String, required: true },
-  rol: { type: String, required: true, enum: ['admin', 'profesor', 'alumno'], default: 'admin' }
->>>>>>> rocio-backend
+  rol: {
+    type: String,
+    required: true,
+    enum: ["admin", "profesor", "alumno"],
+    default: "admin",
+  },
 });
 
-module.exports = mongoose.model('Admin', AdminSchema, 'admin');
+module.exports = mongoose.model("Admin", AdminSchema, "admin");
--- src/models/Alumno.js
+++ src/models/Alumno.js
@@ -1,25 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const AlumnoSchema = new mongoose.Schema({
-<<<<<<< HEAD
-  nombre:    { type: String, required: true },
-  apellidos: { type: String, required: true },
-  email:     { type: String, required: true, unique: true },
-  password:  { type: String, required: true },
-  rol:       { type: String, default: 'alumno' },
-  edad:      { type: Number },
-  campus:    { type: String },
-  cursoId:   { type: mongoose.Schema.Types.ObjectId, ref: 'Curso' }
-=======
   nombre: { type: String, required: true, trim: true },
   apellidos: { type: String, required: true, trim: true },
-  email: { type: String, required: true, unique: true, trim: true},
+  email: { type: String, required: true, unique: true, trim: true },
   password: { type: String, required: true },
-  rol: { type: String, required: true, enum: ['admin', 'profesor', 'alumno'], default: 'alumno' },
+  rol: {
+    type: String,
+    required: true,
+    enum: ["admin", "profesor", "alumno"],
+    default: "alumno",
+  },
   edad: { type: Number },
   campus: { type: String, trim: true },
-  curso: { type: mongoose.Schema.Types.ObjectId, ref: 'Curso' }
->>>>>>> rocio-backend
+  curso: { type: mongoose.Schema.Types.ObjectId, ref: "Curso" },
 });
 
-module.exports = mongoose.model('Alumno', AlumnoSchema, 'alumno');
+module.exports = mongoose.model("Alumno", AlumnoSchema, "alumno");
